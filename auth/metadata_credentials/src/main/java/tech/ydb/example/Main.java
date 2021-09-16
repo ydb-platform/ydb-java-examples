76b0b19795dc6cd431e21b20347f550f2468f117
pnv1 <pnv1@yandex-team.ru>
2021-09-16 14:34:26 +0300

    Java SDK. Add authentication examples for maven, KIKIMR-13015
    ref:8635864
    sync: https://proxy.sandbox.yandex-team.ru/2430330477

---
+++ auth/metadata_credentials/src/main/java/tech/ydb/example/Main.java
@@ -0,0 +1,48 @@
+package tech.ydb.example;
+
+import tech.ydb.auth.iam.CloudAuthProvider;
+import tech.ydb.core.Result;
+import tech.ydb.core.auth.AuthProvider;
+import tech.ydb.core.grpc.GrpcTransport;
+import tech.ydb.table.Session;
+import tech.ydb.table.TableClient;
+import tech.ydb.table.result.ResultSetReader;
+import tech.ydb.table.rpc.grpc.GrpcTableRpc;
+import tech.ydb.table.transaction.TxControl;
+
+import yandex.cloud.sdk.auth.provider.ComputeEngineCredentialProvider;
+
+import java.time.Duration;
+
+public final class Main {
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.err.println("Usage: java -jar example.jar <endpoint> <database>");
+        }
+        String endpoint = args[0];
+        String database = args[1];
+
+        // Use metadata credentials
+        AuthProvider authProvider = CloudAuthProvider.newAuthProvider(
+            ComputeEngineCredentialProvider.builder()
+                .build()
+        );
+
+        GrpcTransport transport = GrpcTransport.forEndpoint(endpoint, database)
+                .withAuthProvider(authProvider)
+                .withSecureConnection()
+                .build();
+        TableClient tableClient = TableClient.newClient(GrpcTableRpc.useTransport(transport))
+                .build();
+        Result<Session> sessionResult = tableClient.getOrCreateSession(Duration.ofSeconds(10))
+                .join();
+        Session session = sessionResult.expect("ok");
+        ResultSetReader rsReader = session.executeDataQuery("SELECT 1;", TxControl.serializableRw()).join()
+                .expect("ok").getResultSet(0);
+        assert(rsReader.getRowCount() == 1);
+        System.out.println("Result:");
+        while (rsReader.next()) {
+            System.out.println(rsReader.getColumn(0).getInt32());
+        }
+    }
+}
