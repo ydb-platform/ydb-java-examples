d14b6cdfa87a88865992c6cd9f9a4f2bb6dc81aa
Alexandr Gorshenin <alexandr268@ydb.tech>
2023-11-08 15:23:03 +0000

    Update readTable example

--- ydb-cookbook/src/main/java/tech/ydb/examples/simple/ReadTableExample.java
+++ ydb-cookbook/src/main/java/tech/ydb/examples/simple/ReadTableExample.java
@@ -1,11 +1,13 @@
 package tech.ydb.examples.simple;
 
 
-import java.time.Duration;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import tech.ydb.core.grpc.GrpcTransport;
 import tech.ydb.examples.SimpleExample;
-import tech.ydb.table.Session;
+import tech.ydb.table.SessionRetryContext;
 import tech.ydb.table.TableClient;
 import tech.ydb.table.result.ResultSetReader;
 import tech.ydb.table.settings.ReadTableSettings;
@@ -17,28 +19,31 @@ import tech.ydb.table.values.PrimitiveValue;
  * @author Sergey Polovko
  */
 public class ReadTableExample extends SimpleExample {
+    private static final String TABLE_NAME = "read_table_example";
+    private static final Logger logger = LoggerFactory.getLogger(ReadTableExample.class);
+
 
     @Override
     protected void run(GrpcTransport transport, String pathPrefix) {
-        try (
-                TableClient tableClient = TableClient.newClient(transport).build();
-                Session session = tableClient.createSession(Duration.ofSeconds(5)).join().getValue()
-                ) {
+        try (TableClient tableClient = TableClient.newClient(transport).build()) {
+            SessionRetryContext retryCtx = SessionRetryContext.create(tableClient).build();
 
-            String tablePath = pathPrefix + getClass().getSimpleName();
-            createAndFillTable(session, tablePath);
-            readTable(session, tablePath);
+            createAndFillTable(retryCtx);
+            readTable(retryCtx, transport.getDatabase());
+            dropTable(retryCtx);
         }
     }
 
-    private void readTable(Session session, String tablePath) {
+    private void readTable(SessionRetryContext retryCtx, String database) {
+
         ReadTableSettings settings = ReadTableSettings.newBuilder()
             .orderedRead(true)
             .fromKeyInclusive(PrimitiveValue.newUint32(10))
             .toKeyExclusive(PrimitiveValue.newUint32(25))
             .build();
 
-        session.executeReadTable(tablePath, settings).start(part -> {
+        String tablePath = database + "/" + TABLE_NAME;
+        retryCtx.supplyStatus(session -> session.executeReadTable(tablePath, settings).start(part -> {
             ResultSetReader resultSet = part.getResultSetReader();
 
             // we are going to read a lot of data, so map column names to indexes
@@ -49,29 +54,35 @@ public class ReadTableExample extends SimpleExample {
             while (resultSet.next()) {
                 long key = resultSet.getColumn(keyIdx).getUint32();
                 String value = resultSet.getColumn(valueIdx).getText();
-                System.out.printf("key=%d, value=%s\n", key, value);
+                logger.info("key={}, value={}", key, value);
             }
-        }).join().expectSuccess("readTable failed");
+        })).join().expectSuccess("readTable failed");
     }
 
-    private void createAndFillTable(Session session, String tablePath) {
+    private void createAndFillTable(SessionRetryContext retryCtx) {
         String createTable =
-            "CREATE TABLE [" + tablePath + "] (" +
+            "CREATE TABLE " + TABLE_NAME + " (" +
             "  key Uint32," +
             "  value Utf8," +
             "  PRIMARY KEY(key)" +
             ");";
 
-        session.executeSchemeQuery(createTable)
+        retryCtx.supplyStatus(session -> session.executeSchemeQuery(createTable))
             .join()
             .expectSuccess("cannot create table");
 
         for (int i = 0; i < 100; i++) {
-            String query = "REPLACE INTO [" + tablePath + "](key, value) VALUES (" + i + ", \"<" + i + ">\");";
-            session.executeDataQuery(query, TxControl.serializableRw().setCommitTx(true))
-                .join()
-                .getStatus().expectSuccess("cannot execute insert");
+            String query = "UPSERT INTO " + TABLE_NAME + "(key, value) VALUES (" + i + ", \"<" + i + ">\");";
+            retryCtx.supplyResult(session -> session.executeDataQuery(query, TxControl.serializableRw()))
+                    .join().getStatus().expectSuccess("cannot execute insert");
         }
+    }
+
+    private void dropTable(SessionRetryContext retryCtx) {
+        String dropSQL = "DROP TABLE " + TABLE_NAME + ";";
+        retryCtx.supplyStatus(session -> session.executeSchemeQuery(dropSQL))
+            .join()
+            .expectSuccess("cannot create table");
     }
 
     public static void main(String[] args) {
