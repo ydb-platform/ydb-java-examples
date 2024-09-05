ecc569f622caae34726c0bee762519cefa9d23d6
Kirill Kurdyukov <kurdyukov-kir@ydb.tech>
2024-09-05 18:35:30 +0300

    feat: simple app spring jooq (#40)

--- jdbc/spring-jooq/src/main/resources/db/migration/V1__schema.sql
+++ jdbc/spring-jooq/src/main/resources/db/migration/V1__schema.sql
@@ -1,28 +1,29 @@
 CREATE TABLE series
 (
-    series_id Int64,
-    title Utf8,
-    series_info Utf8,
+    series_id    Int64,
+    title        Text,
+    series_info  Text,
     release_date Date,
-    PRIMARY KEY (series_id)
+    PRIMARY KEY (series_id),
+    INDEX        title_name GLOBAL ON (title)
 );
 
 CREATE TABLE seasons
 (
-    series_id Int64,
-    season_id Int64,
-    title Utf8,
+    series_id   Int64,
+    season_id   Int64,
+    title       Text,
     first_aired Date,
-    last_aired Date,
+    last_aired  Date,
     PRIMARY KEY (series_id, season_id)
 );
 
 CREATE TABLE episodes
 (
-    series_id Int64,
-    season_id Int64,
+    series_id  Int64,
+    season_id  Int64,
     episode_id Int64,
-    title Utf8,
-    air_date Date,
+    title      Text,
+    air_date   Date,
     PRIMARY KEY (series_id, season_id, episode_id)
 );
