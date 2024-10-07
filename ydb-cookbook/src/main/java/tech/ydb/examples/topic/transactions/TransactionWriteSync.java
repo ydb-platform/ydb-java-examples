adb3720ad36ded7ce2f2415b48a40ce7d94cd1cc
Nikolay Perfilov <pnv1@yandex-team.ru>
2024-10-07 20:49:48 +0300

    Add TransactionWriteSync example

--- ydb-cookbook/src/main/java/tech/ydb/examples/topic/transactions/TransactionWriteSync.java
+++ ydb-cookbook/src/main/java/tech/ydb/examples/topic/transactions/TransactionWriteSync.java
@@ -1,6 +1,5 @@
 package tech.ydb.examples.topic.transactions;
 
-import java.time.Duration;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
@@ -8,14 +7,16 @@ import java.util.concurrent.TimeoutException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import tech.ydb.common.transaction.TxMode;
-import tech.ydb.core.Result;
 import tech.ydb.core.grpc.GrpcTransport;
 import tech.ydb.examples.SimpleExample;
-import tech.ydb.table.Session;
-import tech.ydb.table.TableClient;
-import tech.ydb.table.query.DataQueryResult;
+import tech.ydb.query.QueryClient;
+import tech.ydb.query.QueryStream;
+import tech.ydb.query.QueryTransaction;
+import tech.ydb.query.tools.QueryReader;
+import tech.ydb.query.tools.SessionRetryContext;
+import tech.ydb.table.query.Params;
 import tech.ydb.table.result.ResultSetReader;
-import tech.ydb.table.transaction.TableTransaction;
+import tech.ydb.table.values.PrimitiveValue;
 import tech.ydb.topic.TopicClient;
 import tech.ydb.topic.description.Codec;
 import tech.ydb.topic.settings.SendSettings;
@@ -28,92 +29,98 @@ import tech.ydb.topic.write.SyncWriter;
  */
 public class TransactionWriteSync extends SimpleExample {
     private static final Logger logger = LoggerFactory.getLogger(TransactionWriteSync.class);
+    private static final String PRODUCER_ID = "messageGroup1";
+    private static final String MESSAGE_GROUP_ID = "messageGroup1";
+    // How long should we wait for a message to be put into sending buffer
+    private static final long WRITING_WAIT_TIMEOUT_SECONDS = 5;
+    private static final long SHUTDOWN_TIMEOUT_SECONDS = 10;
 
     @Override
     protected void run(GrpcTransport transport, String pathPrefix) {
-        String producerId = "messageGroup1";
-        String messageGroupId = "messageGroup1";
 
         try (TopicClient topicClient = TopicClient.newClient(transport).build()) {
-            try (TableClient tableClient = TableClient.newClient(transport).build()) {
-                WriterSettings settings = WriterSettings.newBuilder()
+            try (QueryClient queryClient = QueryClient.newClient(transport).build()) {
+                WriterSettings writerSettings = WriterSettings.newBuilder()
                         .setTopicPath(TOPIC_NAME)
-                        .setProducerId(producerId)
-                        .setMessageGroupId(messageGroupId)
+                        .setProducerId(PRODUCER_ID)
+                        .setMessageGroupId(MESSAGE_GROUP_ID)
                         .setCodec(Codec.ZSTD)
                         .setMaxSendBufferMessagesCount(100)
                         .build();
 
-                SyncWriter writer = topicClient.createSyncWriter(settings);
-
-                logger.info("SyncWriter created ");
-
-                try {
-                    writer.initAndWait();
-                    logger.info("Init finished");
-                } catch (Exception exception) {
-                    logger.error("Exception while initializing writer: ", exception);
-                    return;
-                }
-
-                long timeoutSeconds = 5; // How long should we wait for a message to be put into sending buffer
-                // creating session and transaction
-                Result<Session> sessionResult = tableClient.createSession(Duration.ofSeconds(10)).join();
-                if (!sessionResult.isSuccess()) {
-                    logger.error("Couldn't get a session from the pool: {}", sessionResult);
-                    return; // retry or shutdown
-                }
-                Session session = sessionResult.getValue();
-                TableTransaction transaction = session.createNewTransaction(TxMode.SERIALIZABLE_RW);
-
-                // get message text within the transaction
-                Result<DataQueryResult> dataQueryResult = transaction.executeDataQuery("SELECT \"Hello, world!\";")
-                        .join();
-                if (!dataQueryResult.isSuccess()) {
-                    logger.error("Couldn't execute DataQuery: {}", dataQueryResult);
-                    return; // retry or shutdown
-                }
-                ResultSetReader rsReader = dataQueryResult.getValue().getResultSet(0);
-                byte[] message;
-                if (rsReader.next()) {
-                    message = rsReader.getColumn(0).getBytes();
-                } else {
-                    logger.error("Empty DataQuery result");
-                    return; // retry or shutdown
-                }
-                try {
-                    // Non-blocking call
-                    writer.send(
-                            Message.of(message),
-                            SendSettings.newBuilder()
-                                    .setTransaction(transaction)
-                                    .build(),
-                            timeoutSeconds,
-                            TimeUnit.SECONDS
-                    );
-                    logger.info("Message is sent");
-                } catch (TimeoutException exception) {
-                    logger.error("Send queue is full. Couldn't put message into sending queue within {} seconds",
-                            timeoutSeconds);
-                } catch (InterruptedException | ExecutionException exception) {
-                    logger.error("Couldn't put message into sending queue due to exception: ", exception);
-                }
-
-                // flush to wait until the message reach server before commit
-                writer.flush();
-
-                transaction.commit().join();
-
-                long shutdownTimeoutSeconds = 10;
-                try {
-                    writer.shutdown(shutdownTimeoutSeconds, TimeUnit.SECONDS);
-                } catch (TimeoutException exception) {
-                    logger.error("Shutdown was not finished within {} seconds: ", timeoutSeconds, exception);
-                } catch (InterruptedException | ExecutionException exception) {
-                    logger.error("Shutdown was not finished due to exception: ", exception);
-                }
+                tableAndTopicWithinTransaction(topicClient, queryClient, writerSettings, 1);
+                tableAndTopicWithinTransaction(topicClient, queryClient, writerSettings, 2);
             }
         }
+    }
+
+    private void tableAndTopicWithinTransaction(TopicClient topicClient, QueryClient queryClient,
+                                                WriterSettings writerSettings, long id) {
+        SessionRetryContext retryCtx = SessionRetryContext.create(queryClient).build();
+
+        retryCtx.supplyStatus(querySession -> {
+            // Create new transaction object. It is not yet started on server
+            QueryTransaction transaction = querySession.createNewTransaction(TxMode.SERIALIZABLE_RW);
+
+            // Execute a query to start a new transaction
+            QueryStream queryStream = transaction.createQuery(
+                    "DECLARE $id AS Uint64; " +
+                    "SELECT value FROM table WHERE id=$id",
+                    Params.of("$id", PrimitiveValue.newUint64(id)));
+
+            // Get query result
+            QueryReader queryReader = QueryReader.readFrom(queryStream).join().getValue();
+            ResultSetReader resultSet = queryReader.getResultSet(0);
+            if (!resultSet.next()) {
+                throw new RuntimeException("Value for id=" + id + " not found");
+            }
+            String value = resultSet.getColumn("value").getText();
+
+            // Create and initialize a writer
+            SyncWriter writer = topicClient.createSyncWriter(writerSettings);
+            logger.info("SyncWriter created");
+            try {
+                writer.initAndWait();
+                logger.info("SyncWriter initialized");
+            } catch (Exception exception) {
+                throw new RuntimeException("Exception while initializing writer: " + exception);
+            }
+
+            // Write a message
+            try {
+                writer.send(
+                        Message.of(value.getBytes()),
+                        SendSettings.newBuilder()
+                                .setTransaction(transaction)
+                                .build(),
+                        WRITING_WAIT_TIMEOUT_SECONDS,
+                        TimeUnit.SECONDS
+                );
+                logger.info("Message is sent");
+            } catch (TimeoutException exception) {
+                logger.error("Send queue is full. Couldn't put message into sending queue within {} seconds",
+                        WRITING_WAIT_TIMEOUT_SECONDS);
+            } catch (InterruptedException | ExecutionException exception) {
+                logger.error("Couldn't put message into sending queue due to exception: ", exception);
+            }
+
+            // flush to wait until the message will reach the server before committing transaction
+            writer.flush();
+
+            return transaction.commit()
+                    .thenApply(commitResult -> {
+                        try {
+                            writer.shutdown(SHUTDOWN_TIMEOUT_SECONDS, TimeUnit.SECONDS);
+                        } catch (TimeoutException exception) {
+                            throw new RuntimeException("Shutdown not finished within " + SHUTDOWN_TIMEOUT_SECONDS +
+                                    " seconds");
+                        } catch (InterruptedException | ExecutionException exception) {
+                            throw new RuntimeException("Shutdown not finished due to exception: " + exception);
+                        }
+                        // Return commit status to SessionRetryContext function
+                        return commitResult.getStatus();
+                    });
+        }).join().expectSuccess("Can't create table series");
     }
 
     public static void main(String[] args) {
