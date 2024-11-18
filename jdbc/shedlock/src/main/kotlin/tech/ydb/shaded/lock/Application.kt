e36ebc94a4f619581ed59c0a759f3c1612d79095
KirillKurdyukov <kurdyukov-kir@ydb.tech>
2024-11-18 18:52:59 +0300

    feat: updating examples

--- jdbc/shedlock/src/main/kotlin/tech/ydb/shaded/lock/Application.kt
+++ jdbc/shedlock/src/main/kotlin/tech/ydb/shaded/lock/Application.kt
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.scheduling.annotation.EnableScheduling
 
+
 /**
  * @author Kirill Kurdyukov
  */
--- jdbc/shedlock/src/main/kotlin/tech/ydb/shaded/lock/JobHandler.kt
+++ jdbc/shedlock/src/main/kotlin/tech/ydb/shaded/lock/JobHandler.kt
@@ -1,12 +1,9 @@
 package tech.ydb.shaded.lock
 
-import jakarta.annotation.PostConstruct
 import net.javacrumbs.shedlock.spring.annotation.SchedulerLock
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
-import tech.ydb.lock.provider.YdbCoordinationServiceLockProvider
-
+import tech.ydb.lock.provider.YdbJDBCLockProvider
 
 /**
  * @author Kirill Kurdyukov
@@ -15,12 +12,16 @@ import tech.ydb.lock.provider.YdbCoordinationServiceLockProvider
 class JobHandler {
 
     @Scheduled(cron = "* * * * * *")
-    @SchedulerLock(name = "YDB Some Job", lockAtLeastFor = "15S", lockAtMostFor = "20S")
+    @SchedulerLock(name = "YDB Some Job", lockAtMostFor = "20S")
     fun awesomeJob() {
+        println("PID LEADER: " + ProcessHandle.current().pid())
+
         for (i in 0..4) {
             println("Processing {$i}")
 
             Thread.sleep(1_000)
         }
+
+        println("STOP LEADER")
     }
 }
