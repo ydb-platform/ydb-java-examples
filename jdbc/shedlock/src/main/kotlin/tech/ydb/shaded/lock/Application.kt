8222f578511e1aefb1c6c6f14d21fda133803204
KirillKurdyukov <kurdyukov-kir@ydb.tech>
2024-11-18 18:55:13 +0300

    delete whitespace

--- jdbc/shedlock/src/main/kotlin/tech/ydb/shaded/lock/Application.kt
+++ jdbc/shedlock/src/main/kotlin/tech/ydb/shaded/lock/Application.kt
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.scheduling.annotation.EnableScheduling
 
-
 /**
  * @author Kirill Kurdyukov
  */
