4be17038a7d24677318c29d83fd54f7f7a17426e
Kirill Kurdyukov <kurdyukov-kir@ydb.tech>
2024-11-19 11:24:29 +0300

    feat: updating versions (#45)

--- jdbc/shedlock/src/main/kotlin/tech/ydb/shaded/lock/JobHandler.kt
+++ jdbc/shedlock/src/main/kotlin/tech/ydb/shaded/lock/JobHandler.kt
@@ -1,12 +1,8 @@
 package tech.ydb.shaded.lock
 
-import jakarta.annotation.PostConstruct
 import net.javacrumbs.shedlock.spring.annotation.SchedulerLock
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
-import tech.ydb.lock.provider.YdbCoordinationServiceLockProvider
-
 
 /**
  * @author Kirill Kurdyukov
@@ -15,12 +11,16 @@ import tech.ydb.lock.provider.YdbCoordinationServiceLockProvider
 class JobHandler {
 
     @Scheduled(cron = "* * * * * *")
-    @SchedulerLock(name = "YDB Some Job", lockAtLeastFor = "15S", lockAtMostFor = "20S")
+    @SchedulerLock(name = "YDB Some Job", lockAtMostFor = "10S")
     fun awesomeJob() {
+        println("PID LEADER: " + ProcessHandle.current().pid())
+
         for (i in 0..4) {
             println("Processing {$i}")
 
             Thread.sleep(1_000)
         }
+
+        println("STOP LEADER")
     }
 }
