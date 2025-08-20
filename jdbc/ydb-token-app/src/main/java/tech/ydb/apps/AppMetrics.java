7db59aea9216b483bac93d62f4fb0ab58174359f
Alexandr Gorshenin <alexandr268@ydb.tech>
2025-08-20 11:50:06 +0100

    Fixed metric lables

--- jdbc/ydb-token-app/src/main/java/tech/ydb/apps/AppMetrics.java
+++ jdbc/ydb-token-app/src/main/java/tech/ydb/apps/AppMetrics.java
@@ -1,7 +1,6 @@
 package tech.ydb.apps;
 
 import java.sql.SQLException;
-import java.time.Duration;
 import java.util.Arrays;
 import java.util.EnumMap;
 import java.util.Map;
@@ -14,8 +13,8 @@ import java.util.concurrent.atomic.LongAdder;
 import java.util.function.Function;
 
 import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.DistributionSummary;
 import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.core.instrument.Timer;
 import org.slf4j.Logger;
 import org.springframework.retry.RetryCallback;
 import org.springframework.retry.RetryContext;
@@ -36,23 +35,10 @@ public class AppMetrics {
     private static final Counter.Builder SDK_OPERATIONS_FAILTURE = Counter.builder("sdk.operations.failture");
     private static final Counter.Builder SDK_RETRY_ATTEMPS = Counter.builder("sdk.retry.attempts");
 
-    private static final Timer.Builder SDK_OPERATION_LATENCY = Timer.builder("sdk.operation.latency")
-            .sla(
-                    Duration.ofMillis(1),
-                    Duration.ofMillis(2),
-                    Duration.ofMillis(3),
-                    Duration.ofMillis(4),
-                    Duration.ofMillis(5),
-                    Duration.ofMillis(8),
-                    Duration.ofMillis(10),
-                    Duration.ofMillis(20),
-                    Duration.ofMillis(50),
-                    Duration.ofMillis(100),
-                    Duration.ofMillis(200),
-                    Duration.ofMillis(500),
-                    Duration.ofMillis(1000)
-            );
-//            .publishPercentiles();
+//    private static final Timer.Builder SDK_OPERATION_LATENCY = Timer.builder("sdk.operation.latency")
+    private static final DistributionSummary.Builder SDK_OPERATION_LATENCY = DistributionSummary
+            .builder("sdk.operation.latency")
+            .serviceLevelObjectives(0.001, 0.002, 0.003, 0.004, 0.005, 0.0075, 0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1);
 
     public class Method {
         private final String name;
@@ -67,25 +53,31 @@ public class AppMetrics {
         private final Counter successCounter;
         private final Map<StatusCode, Counter> errorsCountersMap = new EnumMap<>(StatusCode.class);
         private final Map<StatusCode, Counter> retriesCountersMap = new EnumMap<>(StatusCode.class);
-        private final Map<StatusCode, Timer> durationTimerMap = new EnumMap<>(StatusCode.class);
+        private final Map<StatusCode, DistributionSummary> durationTimerMap = new EnumMap<>(StatusCode.class);
         private final Function<StatusCode, Counter> errorCounter;
         private final Function<StatusCode, Counter> retriesCounter;
-        private final Function<StatusCode, Timer> durationTimer;
+        private final Function<StatusCode, DistributionSummary> durationTimer;
 
         private volatile long lastPrinted = 0;
 
         public Method(MeterRegistry registry, String name, String label) {
             this.name = name;
-            this.executionsCounter = SDK_OPERATIONS.tag("method", label).register(registry);
-            this.successCounter = SDK_OPERATIONS_SUCCESS.tag("method", label).register(registry);
-            this.errorCounter = code -> errorsCountersMap.computeIfAbsent(code, key ->
-                    SDK_OPERATIONS_FAILTURE.tag("method", label).tag("status", key.toString()).register(registry)
+            this.executionsCounter = SDK_OPERATIONS.tag("operation_type", label).register(registry);
+            this.successCounter = SDK_OPERATIONS_SUCCESS.tag("operation_type", label).register(registry);
+            this.errorCounter = code -> errorsCountersMap.computeIfAbsent(code, key -> SDK_OPERATIONS_FAILTURE
+                    .tag("operation_type", label)
+                    .tag("operation_status", key.toString())
+                    .register(registry)
             );
-            this.retriesCounter = code -> retriesCountersMap.computeIfAbsent(code, key ->
-                    SDK_RETRY_ATTEMPS.tag("method", label).tag("status", key.toString()).register(registry)
+            this.retriesCounter = code -> retriesCountersMap.computeIfAbsent(code, key -> SDK_RETRY_ATTEMPS
+                    .tag("operation_type", label)
+                    .tag("operation_status", key.toString())
+                    .register(registry)
             );
-            this.durationTimer = code -> durationTimerMap.computeIfAbsent(code, key ->
-                    SDK_OPERATION_LATENCY.tag("method", label).tag("status", key.toString()).register(registry)
+            this.durationTimer = code -> durationTimerMap.computeIfAbsent(code, key -> SDK_OPERATION_LATENCY
+                    .tag("operation_type", label)
+                    .tag("operation_status", key.toString())
+                    .register(registry)
             );
         }
 
@@ -112,13 +104,15 @@ public class AppMetrics {
                 timeMs.add(ms);
                 totalTimeMs.add(ms);
 
-                durationTimer.apply(code).record(ms, TimeUnit.MILLISECONDS);
+                durationTimer.apply(code).record(0.001 * ms);
             }
         }
 
         public void close() {
             successCounter.close();
             executionsCounter.close();
+            durationTimerMap.forEach((status, counter) -> counter.close());
+            retriesCountersMap.forEach((status, counter) -> counter.close());
             errorsCountersMap.forEach((status, counter) -> counter.close());
         }
 
